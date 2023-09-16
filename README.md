fd683c7beeac8cd6962c320d1353cfcdfcf56c13
kazuki1023 <k.keio12569@gmail.com>
2023-09-16 14:55:12 +0900

    tailwind構築完了

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## After Party
+
+
+### tailwind
+
+```
+docker compose exec node sh
+```
+
+```
+npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+```
---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.php",
+    "./assets/php/*.php",
+    "./assets/php/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
