5446fb6899348243089d17723cb90cb1db36982b
kazuki1023 <k.keio12569@gmail.com>
2023-09-18 00:22:07 +0900

    add want to のメモ

--- src/index.php
+++ src/index.php
@@ -100,6 +100,7 @@ try {
   </form>
 
   <?php if(empty($results)) {?>
+    <!-- ここにalertで誰もいないこと伝えたいな -->
     <?php } else {?>
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
