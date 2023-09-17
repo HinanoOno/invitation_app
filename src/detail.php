92c58f047de3d61d783507618cbeb0df077475ea
kazuki1023 <k.keio12569@gmail.com>
2023-09-18 01:20:20 +0900

    add detail

---
+++ src/detail.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once('./assets/php/detail.php');
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../assets/style/reset.css">
+  <link rel="stylesheet" href="../dist/output.css">
+</head>
+<body>
+  
+</body>
+</html>
