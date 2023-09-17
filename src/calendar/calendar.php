9a4adc7701387ee8a86ec78de08d5c161cf79f68
HinanoOno <hinagon1231@icloud.com>
2023-09-17 22:14:09 +0900

    個別通知

--- src/calendar/calendar.php
+++ src/calendar/calendar.php
@@ -252,7 +252,7 @@ echo '</pre>';
                   <label for="countries_multiple" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                   <select multiple id="countries_multiple" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="name[]">
                     <?php foreach ($users as $user) : ?>
-                      <option value="<?php echo $user['name']; ?>"><?php echo $user['name']; ?></option>
+                      <option  value="<?php echo $user['name']; ?>"><?php echo $user['name']; ?></option>
                     <?php endforeach; ?>
                   </select>
                   <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
