543282fd425f545c69eb37da09a0e1d7da03f67e
HinanoOno <hinagon1231@icloud.com>
2023-09-17 22:26:55 +0900

    通知機能

--- src/assets/php/index.php
+++ src/assets/php/index.php
@@ -10,22 +10,50 @@
     // 入室・退室判断
     if ($_POST['status'] === '入室') {
       $status = 1;
-
       try {
-          $dbh->beginTransaction();
+        $dbh->beginTransaction();
 
-          foreach ($selectedValues as $value) {
-              $sql = "INSERT INTO userDetail_plan (userDetail_id, plan_id, status) VALUES (:userDetail_id, :plan_id, :status)";
-              $stmt = $dbh->prepare($sql);
-              $stmt->execute([
-                  "userDetail_id" => $_SESSION['id'],
-                  "plan_id" => $value,
-                  "status" => $status
-              ]);
-          }
-          $dbh->commit();
-          header("Location: ". "/index.php");
-          exit();
+        foreach ($selectedValues as $value) {
+            $sql = "INSERT INTO userDetail_plan (userDetail_id, plan_id, status) VALUES (:userDetail_id, :plan_id, :status)";
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute([
+                "userDetail_id" => $_SESSION['id'],
+                "plan_id" => $value,
+                "status" => $status
+            ]);
+        }
+        $sql = "SELECT name FROM user_details WHERE id = :userID";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(":userID", $_SESSION['id'], PDO::PARAM_INT);
+        $stmt->execute();
+        $user_name = $stmt->fetch(PDO::FETCH_ASSOC);
+        $dbh->commit();
+        // Discord通知を行う先
+        $discordWebhookURL = "https://discord.com/api/webhooks/1152754404156510208/VXcIz8LVxBbNaXViJrp9aVH5TjXR_nV7CHUmld2bVtBsuapal7BHWMU2xM1SzY6OIwcX";
+        $message = $user_name['name']."さんが入室しました"; 
+
+        $data = [
+            "content" => $message
+        ];
+
+        $options = [
+            "http" => [
+                "header" => "Content-Type: application/json",
+                "method" => "POST",
+                "content" => json_encode($data)
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($discordWebhookURL, false, $context);
+
+        if ($result === FALSE) {
+            echo "Discord通知の送信に失敗しました。";
+        } else {
+            echo "データが登録され、Discordに通知が送信されました。";
+        }  
+        header("Location: ". "/index.php");
+        exit();
       } catch (PDOException $e) {
           $dbh->rollBack();
           echo "データベースエラー: " . $e->getMessage();
